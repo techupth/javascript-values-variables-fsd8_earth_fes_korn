51c2362aea263cf6c30e3446ad27b3c2588be19a
Suriya <true.many.032@gmail.com>
2025-02-24 13:58:52 +0700

    Assignment 1

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
-//Exercise #1: Variable Creation
-//Start coding here
+// Ex1
+let Name = "Karter";
+let LastName = "Anderson";
+let Address = "52/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const RestaurantName = "Chef's Table by Chef James";
+const TotalBill = 15000;
+const CurrentOpenRestaurant = 1500;
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+// Ex2
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+TotalBill = 5000;
+CurrentOpenRestaurant = 2500;
--- tests/ex-3.test.js
+++ tests/ex-3.test.js
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 describe("Exercise 3", () => {
   it("ค่า fahrenheit จะต้องเป็นค่าที่ถูกต้อง", async () => {
     const data = await fs.readFile("./ex-3.js");
-
+    
     const code = `${data} 
     return fahrenheit;`;
 
