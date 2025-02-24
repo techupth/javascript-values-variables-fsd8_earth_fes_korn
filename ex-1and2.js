8298a74f642cf43f3f212e6047c1551685c42719
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-02-24 14:06:49 +0700

    ex1234

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurang = "Chef's Table by Chef James"
+const amountBills = 15000;
+const numberMenu = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+amountBills = 5000;
+numberMenu = 2500;
+
+
+
+
+
